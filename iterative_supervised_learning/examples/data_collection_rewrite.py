3bf4c1744e74563846bf02479a867c33720feffd
Chi Zhang <zhangchi9744@gmail.com>
2025-02-24 16:57:44 +0000

    clean up and rewrite data_collection & rollout_mpc

---
+++ iterative_supervised_learning/examples/data_collection_rewrite.py
@@ -0,0 +1,122 @@
+## NOTE: This script is for data collection for behavior cloning.
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
+
+from omegaconf import OmegaConf
+import argparse
+from typing import Tuple, List
+import os
+import numpy as np
+from datetime import datetime
+
+# from iterative_supervised_learning.utils.RolloutMPC import RolloutMPC
+from iterative_supervised_learning.utils.RolloutMPC_rewrite import rollout_mpc
+from iterative_supervised_learning.utils.database import Database
+import random
+import hydra
+import h5py
+import pickle
+import scipy.spatial.transform as st
+import pinocchio as pin
+from mj_pin.utils import get_robot_description
+
+class DataCollection():
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.episode_length = cfg.episode_length
+        self.sim_dt = cfg.sim_dt
+        self.n_iteration = cfg.n_iteration
+        self.num_pertubations_per_replanning = cfg.num_pertubations_per_replanning
+        
+        self.gaits = cfg.gaits
+        self.vx_range = (cfg.vx_des_min, cfg.vx_des_max)
+        self.vy_range = (cfg.vy_des_min, cfg.vy_des_max)
+        self.w_range = (cfg.w_des_min, cfg.w_des_max)
+        self.database = Database(limit=cfg.database_size,norm_input=False)
+        self.data_save_path = self._prepare_save_path()
+        
+    
+    def _prepare_save_path(self):
+        current_time = datetime.now().strftime("%b_%d_%Y_%H_%M_%S")
+        base_path = f"{self.cfg.data_save_path}/behavior_cloning/{'_'.join(self.gaits)}/"
+        if self.cfg.suffix:
+            base_path += f"_{self.cfg.suffix}/"
+        return os.path.join(base_path, current_time, "dataset")
+    
+    def save_dataset(self, iteration):
+        os.makedirs(self.data_save_path, exist_ok=True)
+        
+        data_len = len(self.database)
+        with h5py.File(f"{self.data_save_path}/database_{iteration}.hdf5", 'w') as hf:
+            # print(f"Final states shape: {self.database.states.shape}, Type: {type(self.database.states)}")
+            hf.create_dataset('states', data=self.database.states[:data_len])
+            hf.create_dataset('vc_goals', data=self.database.vc_goals[:data_len])
+            hf.create_dataset('cc_goals', data=self.database.cc_goals[:data_len])
+            hf.create_dataset('actions', data=self.database.actions[:data_len]) 
+            
+        config_path = f"{self.data_save_path}/config.pkl"
+        if not os.path.exists(config_path):
+            with open(config_path, "wb") as f:
+                pickle.dump(self.cfg, f)
+        print(f"Dataset saved at iteration {iteration+1}")
+
+    def run(self):
+        nq = 19
+        nv = 17
+        replan_freq = 2000
+        n_state = 44
+        
+        # rollout nominal trajectory
+        _, record_path_nominal = rollout_mpc(show_plot=False,
+                                         v_des = [0.3,0.0,0.0],
+                                         sim_time=4.0)
+        
+        # calculate replanning points
+        replanning_points = np.arange(0, self.episode_length, replan_freq)
+        print("Replanning points:", replanning_points)
+        
+        # extract nominal state on replanning points
+        data = np.load(record_path_nominal)
+        state = data["state"]
+        # print(state[:2])
+        # input()
+        
+        phase_percentage = state[:,0]
+        nominal_v = data["v"]
+        nominal_q = data["q"]
+        # print("shape of v is = ",np.shape(nominal_v))
+        # print("shape of q is = ",np.shape(nominal_q))
+        # print("shape of phase_percentage is = ", np.shape(phase_percentage) )
+        # input()
+        vc_goals = data["vc_goals"][0]
+        cc_goals = None
+        actions = data["ctrl"]
+        
+        # rollout MPC at each replanning point
+        for i_replanning in replanning_points:
+            print(f"Replanning at step {i_replanning}")
+            q0 = nominal_q[i_replanning]
+            v0 = nominal_v[i_replanning]
+            for j in range(self.num_pertubations_per_replanning):
+                # randomize on given state and pass to mpc simulator
+                randomize_on_given_state = np.concatenate((q0, v0, np.array([phase_percentage[i_replanning]])))
+                early_termination = False
+                # run MPC from replanning state until the simulation finishes
+                while True:
+                    early_termination, record_path_replanning = rollout_mpc(randomize_on_given_state=randomize_on_given_state, 
+                                                                            v_des=[0.3,0.0,0.0],
+                                                                            sim_time=4.0)
+                    if not early_termination:
+                        break
+                    
+                
+# Example usage with database
+@hydra.main(config_path='cfgs', config_name='data_collection_config.yaml',version_base="1.1")
+def main(cfg):
+    dc = DataCollection(cfg)
+    dc.run()
+
+if __name__ == '__main__':
+    main()
+
---
+++ iterative_supervised_learning/utils/RolloutMPC_rewrite.py
@@ -0,0 +1,285 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
+from typing import Tuple, List
+import pinocchio as pin
+import numpy as np
+from mj_pin.abstract import VisualCallback, DataRecorder
+from mj_pin.simulator import Simulator  # type: ignore
+from mj_pin.utils import get_robot_description, mj_frame_pos # type: ignore
+from mpc_controller.mpc import LocomotionMPC
+import scipy.spatial.transform as st
+import mujoco 
+
+# initialize global variables
+SIM_DT = 1.0e-3
+VIEWER_DT = 1/30.
+gait_period = 0.5 # trotting
+n_state = 44
+nq = 19
+nv = 17
+
+# initialize pertubation variables
+mu_base_pos = 0.0
+sigma_base_pos = 0.1
+mu_joint_pos = 0.0
+sigma_joint_pos = 0.2
+mu_base_ori = 0.0
+sigma_base_ori = 0.1
+mu_vel = 0.0
+sigma_vel = 0.1
+
+# VisualCallback
+class ReferenceVisualCallback(VisualCallback):
+    def __init__(self, mpc_controller, update_step = 1):
+        super().__init__(update_step)
+        self.mpc = mpc_controller
+        self.radius = 0.01
+
+    def add_visuals(self, mj_data):
+        # Contact locations
+        for i, foot_cnt in enumerate(self.mpc.solver.dyn.feet):
+            cnt_pos = self.mpc.solver.params[foot_cnt.plane_point.name]
+            cnt_pos_unique = np.unique(cnt_pos, axis=1).T
+            for pos in cnt_pos_unique:
+                if np.sum(pos) == 0.: continue
+                self.add_sphere(pos, self.radius, self.colors.id(i))
+
+        # Base reference
+        BLACK = self.colors.name("black")
+        base_ref = self.mpc.solver.cost_ref[self.mpc.solver.dyn.base_cost.name][:, 0]
+        R_WB = pin.rpy.rpyToMatrix(base_ref[3:6][::-1]).flatten()
+        self.add_box(base_ref[:3], rot=R_WB, size=[0.08, 0.04, 0.04], rgba=BLACK)
+        
+        # Base terminal reference
+        base_ref = self.mpc.solver.cost_ref_terminal[self.mpc.solver.dyn.base_cost.name]
+        R_WB = pin.rpy.rpyToMatrix(base_ref[3:6][::-1]).flatten()
+        self.add_box(base_ref[:3], rot=R_WB, size=[0.08, 0.04, 0.04], rgba=BLACK)
+        
+# Data recorder
+class StateDataRecorder(DataRecorder):
+    def __init__(
+        self,
+        record_dir: str = "",
+        record_step: int = 1,
+        v_des: np.ndarray = np.array([0,0,0])) -> None:
+        """
+        A simple data recorder that saves simulation data to a .npz file.
+        """
+        super().__init__(record_dir, record_step)
+        self.data = {}
+        self.vc_goals = v_des
+        
+        # some initialization
+        self.feet_names = ["FL", "FR", "RL", "RR"]
+        self.robot_name = "go2"
+        xml_path = get_robot_description(self.robot_name).xml_path
+        self.mj_model = mujoco.MjModel.from_xml_path(xml_path)
+        
+        # reset
+        self.reset()
+
+    def reset(self) -> None:
+        self.data = {"time": [], 
+                     "q": [], 
+                     "v": [], 
+                     "ctrl": [],
+                     "feet_pos_w":[],
+                     "base_wrt_feet":[],
+                     "state":[],
+                     "vc_goals":[]}
+
+    def save(self) -> None:
+        if not self.record_dir:
+            self.record_dir = os.getcwd()
+        os.makedirs(self.record_dir, exist_ok=True)
+
+        timestamp = self.get_date_time_str()
+        file_path = os.path.join(self.record_dir, f"simulation_data_{timestamp}.npz")
+
+        try:
+            # Uncomment to save data
+            np.savez(file_path, **self.data)
+            print(f"Data successfully saved to {file_path}")
+            return file_path
+        except Exception as e:
+            print(f"Error saving data: {e}")
+            return ""
+
+    def record(self, mj_data) -> None:
+        """
+        Record simulation data at the current simulation step.
+        """
+        # Record time and state
+        q = mj_data.qpos.copy()
+        v = mj_data.qvel.copy()
+        self.data["time"].append(round(mj_data.time, 4))
+        self.data["q"].append(q)
+        self.data["v"].append(v)
+        self.data["ctrl"].append(mj_data.ctrl.copy())
+        
+        # # Record feet position in the world
+        feet_pos_all = []
+        base_wrt_feet = np.zeros(2*len(self.feet_names))
+        
+        for i, f_name in enumerate(self.feet_names):
+            feet_pos = mj_frame_pos(self.mj_model, mj_data, f_name)
+            # print("feet_pos = ",feet_pos)
+            # print("base_pos = ",q[:3])
+            feet_pos_all.extend(feet_pos)
+            base_wrt_feet[2*i:2*i+2] = (q[:3] - feet_pos)[:2]  # Correct indexing
+        
+        # print("base_wrt_feet = ", base_wrt_feet)
+        # input()
+        self.data["feet_pos_w"].append(np.array(feet_pos_all))
+        self.data["base_wrt_feet"].append(np.array(base_wrt_feet))
+        
+        ## form state variable
+        # the format of state = [[phase_percentage],v,q[2:],base_wrt_feet]
+        phase_percentage = np.round([get_phase_percentage(mj_data.time)], 4)
+        state = np.concatenate([phase_percentage, v, q[2:], base_wrt_feet])
+        self.data["state"].append(np.array(state))
+        
+        # record the desired velocity
+        self.data["vc_goals"].append(self.vc_goals)
+
+def get_phase_percentage(t:int):
+    """get current gait phase percentage based on gait period
+
+    Args:
+        t (int): current sim step (NOT sim time in seconds!)
+
+    Returns:
+        phi: current gait phase. between 0 - 1
+    """ 
+       
+    # for trot
+    gait_period = 0.5
+    phi = (t % gait_period)/gait_period
+    return phi
+
+def random_quaternion_perturbation(sigma):
+    """
+    Generate a small random quaternion perturbation.
+    The perturbation is sampled from a normal distribution with standard deviation sigma.
+    """
+    random_axis = np.random.normal(0, 1, 3)  # Random rotation axis
+    random_axis /= np.linalg.norm(random_axis)  # Normalize to unit vector
+    angle = np.random.normal(0, sigma)  # Small random rotation angle
+    perturb_quat = st.Rotation.from_rotvec(angle * random_axis).as_quat()  # Convert to quaternion
+    return perturb_quat
+
+def apply_quaternion_perturbation(nominal_quat, sigma_base_ori):
+    """
+    Apply a small random rotation perturbation to a given quaternion.
+    """
+    perturb_quat = random_quaternion_perturbation(sigma_base_ori)
+    perturbed_quat = st.Rotation.from_quat(nominal_quat) * st.Rotation.from_quat(perturb_quat)
+    return perturbed_quat.as_quat()  # Convert back to quaternion
+
+def rollout_mpc(robot_name = "go2",
+                interactive = False,
+                v_des = [0.3,0,0],
+                save_data = True,
+                record_dir = "./data",
+                sim_time = 5.0,
+                visualize = True,
+                show_plot = True,
+                randomize_on_given_state = None):
+    # init robot description
+    robot_desc = get_robot_description(robot_name)
+    feet_frame_names = ["FL_foot", "FR_foot", "RL_foot", "RR_foot"]
+    feet_name = ["FL", "FR", "RL", "RR"] 
+    # initialize mpc controller
+    mpc = LocomotionMPC(
+            path_urdf=robot_desc.urdf_path,
+            feet_frame_names=feet_frame_names,
+            robot_name=robot_name,
+            joint_ref=robot_desc.q0,
+            interactive_goal=interactive,
+            sim_dt=SIM_DT,
+            print_info=False,
+            solve_async=True,
+        )
+    
+    if not interactive:
+        mpc.set_command(v_des,0.0)
+        
+    #initialize visual callback
+    vis_feet_pos = ReferenceVisualCallback(mpc)
+    
+    #initialize data recorder
+    data_recorder = StateDataRecorder(record_dir,v_des=v_des) if save_data else None
+    
+    #initialize simulator
+    sim = Simulator(robot_desc.xml_scene_path,sim_dt=SIM_DT,viewer_dt=VIEWER_DT)
+    sim.vs.track_obj = "base"
+    
+    #get default position and velocity
+    q_mj = robot_desc.q0
+    v_mj = np.zeros(mpc.pin_model.nv)
+    
+    # TODO: randomize on given state
+    if randomize_on_given_state is not None:
+        nominal_state = randomize_on_given_state
+        # xml_path = get_robot_description(robot_name).xml_path
+        # mj_model = mujoco.MjModel.from_xml_path(xml_path)
+        
+        while True:
+            q_mj = nominal_state[:nq] 
+            v_mj = nominal_state[nq:-1]
+            
+            # apply pertubation on quatenion
+            nominal_quat = q_mj[3:7]
+            perturbed_quat = apply_quaternion_perturbation(nominal_quat, sigma_base_ori)
+
+            perturbation_q = np.concatenate((np.random.normal(mu_base_pos, sigma_base_pos, 3),\
+                                                        perturbed_quat ,\
+                                                        np.random.normal(mu_joint_pos,sigma_joint_pos,len(q_mj)-7)))
+            perturbation_v = np.random.normal(mu_vel,sigma_vel,len(v_mj))
+            
+            q_mj += perturbation_q
+            v_mj += perturbation_v
+            
+            q_pin, v_pin = mpc.solver.dyn.convert_from_mujoco(q_mj, v_mj)
+            mpc.solver.dyn.update_pin(q_pin, v_pin)
+            feet_pos = mpc.solver.dyn.get_feet_position_w()
+            # feet_pos = mj_frame_pos(mj_model, sim.mj_data, feet_frame_names)
+            if np.all(feet_pos[:,-1] >= 0):
+                print("feet_pos = ", feet_pos)
+                input()
+                break
+    
+    # TODO: maybe there is a better randomization method
+            
+    # Set randomized state and simulate from there
+    sim.set_initial_state(q0=q_mj,v0=v_mj)
+    
+    sim.run(
+        sim_time = sim_time,
+        controller=mpc,
+        visual_callback=vis_feet_pos,
+        data_recorder=data_recorder,
+        use_viewer=visualize,
+        allowed_collision=["FL", "FR", "RL", "RR","floor"]
+    )
+    
+    early_termination = sim.time < sim_time
+        
+    if show_plot:
+        mpc.print_timings()
+        mpc.plot_traj("q")
+        mpc.plot_traj("f")
+        mpc.plot_traj("tau")
+        mpc.show_plots()
+    
+    record_path = ""
+    if save_data and data_recorder:
+        record_path = data_recorder.save()
+        # print(f"Data recorded at: {record_path}")
+    
+    return early_termination, record_path
+
+if __name__ == "__main__":
+    record_dir = rollout_mpc(show_plot=False)
+    print(record_dir)
